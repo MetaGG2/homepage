11f85ec8f5cbfe9d0406d8eee5a03234840142d6
MonkeyGG2 <spinosaurus882@gmail.com>
2022-10-24 11:01:35 -0400

    joe

---
+++ js/time.js
@@ -0,0 +1,17 @@
+var time = null;
+var date = null;
+
+setTime();
+setInterval(setTime, 1000);
+setDate();
+setInterval(setDate, 86400000);
+
+function setTime() {
+    time = new Date().toLocaleTimeString();
+    document.getElementById("time").textContent = new Date().toLocaleTimeString();
+}
+
+function setDate() {
+    date = new Date().toDateString();
+    document.getElementById("date").textContent = date.slice(4);
+}
